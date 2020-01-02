f583b67cf1459d5dcfd26137db43fe2c0edc6d54
JoykishanSharma <joykishan120@gmail.com>
2020-01-02 18:17:51 +0530

    Bird Shooting Game for Android

---
+++ app/src/main/java/com/joyappsdevteam/fightingbirds/Bullet.java
@@ -0,0 +1,39 @@
+package com.joyappsdevteam.fightingbirds;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+
+import static com.joyappsdevteam.fightingbirds.GameView.screenRatioX;
+import static com.joyappsdevteam.fightingbirds.GameView.screenRatioY;
+
+
+public class Bullet {
+
+    int x, y, width, height;
+    Bitmap bullet;
+
+    Bullet(Resources res) {
+
+        bullet = BitmapFactory.decodeResource(res, R.drawable.bullet);
+
+        width = bullet.getWidth();
+        height = bullet.getHeight();
+
+        width /= 4;
+        height /= 4;
+
+        width *= (int) screenRatioX;
+        height *= (int) screenRatioY;
+
+        bullet = Bitmap.createScaledBitmap(bullet, width, height, false);
+
+    }
+
+    Rect getCollisionShape() {
+        return new Rect(x, y, x + width, y + height);
+    }
+
+}
+
